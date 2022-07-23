a4f71c3bbd35144a5a66188c3539e83a66e05bf6
Jesus Campos <jesus.campos123@icloud.com>
2022-07-22 23:16:26 -0500

    fish changes

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,7 @@ alias ls "exa --group-directories-first"
 alias la "exa -a --group-directories-first --header --long"
 alias tree "exa -T"
 alias dotfiles "git --git-dir $HOME/.dotfiles/ --work-tree $HOME"
-alias vim "nvim"
+alias vim nvim
 alias rm "rm -i"
 alias sail='[ -f sail ] && bash sail || bash vendor/bin/sail'
 
@@ -16,7 +16,7 @@ starship init fish | source
 
 # Env variables
 export EDITOR=nvim
-export BROWSER=chromium
+export BROWSER=google-chrome-stable
 export CLIPMAN_IMG=$HOME/.cache/xfce4/clipman/image0.png
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse --border'
 export FZF_DEFAULT_COMMAND='find -L'
