54a90dd719b1c34bd7040a678b0543ac9ae26950
Jesús Campos <jesus.campos123@icloud.com>
2025-05-28 00:17:07 -0500

    Add .angular to be excluded by snacks

--- .config/nvim/lua/plugins/lazyvim/utils/snacks.lua
+++ .config/nvim/lua/plugins/lazyvim/utils/snacks.lua
@@ -8,7 +8,7 @@ return {
     opts.picker.sources.files = {
       hidden = true,
       ignored = true,
-      exclude = { ".git", "node_modules" },
+      exclude = { ".git", "node_modules", ".angular" },
     }
 
     opts.picker.sources.explorer = {
