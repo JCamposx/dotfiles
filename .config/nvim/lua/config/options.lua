c05dbf8ac13de1b144f65797c6e04fc782f47db8
Jesús Campos <jesus.campos123@icloud.com>
2025-06-01 11:18:01 -0500

    Enable word wrapping

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -3,3 +3,8 @@ vim.g.editorconfig = false
 
 -- Disable animations
 vim.g.snacks_animate = false
+
+-- Enable word wrapping
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.linebreak = true
