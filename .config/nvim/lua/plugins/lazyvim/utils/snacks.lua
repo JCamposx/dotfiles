40b806ba1e046e1188a57bd4b1d269517cc2678f
Jesús Campos <jesus.campos123@icloud.com>
2025-06-01 21:35:28 -0500

    Refactor snacks setup

--- .config/nvim/lua/plugins/lazyvim/utils/snacks.lua
+++ .config/nvim/lua/plugins/lazyvim/utils/snacks.lua
@@ -1,25 +1,24 @@
 -- Snacks customization
 return {
   "folke/snacks.nvim",
-  opts = function(_, opts)
-    opts.picker = opts.picker or {}
-    opts.picker.sources = opts.picker.sources or {}
-
-    opts.picker.sources.files = {
-      hidden = true,
-      ignored = true,
-      exclude = { ".git", "node_modules", ".angular" },
-    }
-
-    opts.picker.sources.explorer = {
-      layout = { layout = { position = "right" } },
-      hidden = true,
-      ignored = true,
-      auto_close = true,
-    }
-
-    return opts
-  end,
+  opts = {
+    picker = {
+      sources = {
+        files = {
+          hidden = true,
+          ignored = true,
+          exclude = { ".git", "node_modules", ".angular", "dist" },
+        },
+        explorer = {
+          replace_netrw = true,
+          layout = { layout = { position = "right" } },
+          hidden = true,
+          ignored = true,
+          auto_close = true,
+        },
+      },
+    },
+  },
   -- stylua: ignore
   keys = {
     { "<leader>e", function() Snacks.explorer() end, desc = "Explorer" },
