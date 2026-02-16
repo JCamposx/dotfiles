d4bea6c5445b0e4a0b48fbd36bfb95645f7bd597
Jesús Campos <jesus.campos123@icloud.com>
2026-02-15 21:36:58 -0500

    Update fzf default command

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -1,6 +1,6 @@
 set _fzf_fd_base "fd --hidden --exclude .git --exclude node_modules"
 
-set -gx FZF_DEFAULT_COMMAND "$_fzf_fd_base --type f"
+set -gx FZF_DEFAULT_COMMAND "$_fzf_fd_base"
 set -gx FZF_DEFAULT_OPTS "--height 50% --layout reverse --border"
 set -gx FZF_CTRL_T_COMMAND "$_fzf_fd_base --type f"
 set -gx FZF_CTRL_T_OPTS "--preview 'bat --color always {}' --bind 'enter:execute(nvim {})+abort'"
