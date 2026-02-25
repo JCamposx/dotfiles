ced09cbf56348a8a936c29048f11593c1a603e2e
Jesús Campos <jesus.campos123@icloud.com>
2026-02-24 22:55:38 -0500

    Update keymap configuration in blink.cmp setup

--- .config/nvim/lua/plugins/lazyvim/extras/coding/blink-cmp.lua
+++ .config/nvim/lua/plugins/lazyvim/extras/coding/blink-cmp.lua
@@ -1,6 +1,18 @@
+-- Blink.CMP customization
 return {
   "saghen/blink.cmp",
   opts = {
+    keymap = {
+      preset = "super-tab",
+      ["<C-k>"] = { "select_prev", "fallback_to_mappings" },
+      ["<C-j>"] = { "select_next", "fallback_to_mappings" },
+    },
+    cmdline = {
+      enabled = true,
+      keymap = {
+        preset = "inherit",
+      },
+    },
     completion = {
       menu = {
         winhighlight = "Normal:BlinkCmpDoc,FloatBorder:BlinkCmpDocBorder,CursorLine:BlinkCmpDocCursorLine,Search:None",
