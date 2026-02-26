6d7bb8b7db7b832cb034e6d2cf9b1127fad216b5
Jesús Campos <jesus.campos123@icloud.com>
2026-02-25 20:30:59 -0500

    Update keymap configuration for commasemi in nvim

--- .config/nvim/lua/plugins/user/commasemi.lua
+++ .config/nvim/lua/plugins/user/commasemi.lua
@@ -2,9 +2,14 @@
 return {
   "saifulapm/commasemi.nvim",
   event = "VeryLazy",
+  keys = {
+    { "<A-,>", "<cmd>CommaToggle<CR>", desc = "Toggle comma", mode = { "n", "i" } },
+    { "<A-,>", ":CommaToggle<CR>gv", desc = "Toggle comma", mode = "x" },
+    { "<A-;>", "<cmd>SemiToggle<CR>", desc = "Toggle semicolon", mode = { "n", "i" } },
+    { "<A-;>", ":SemiToggle<CR>gv", desc = "Toggle comma in visual mode", mode = { "x" } },
+  },
   opts = {
-    leader = "<leader>",
-    keymaps = true,
+    keymaps = false,
     commands = true,
   },
 }
