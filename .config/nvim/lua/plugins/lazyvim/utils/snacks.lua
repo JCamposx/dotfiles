008c9abde71e9a337775f1ca392045a232dc49cf
Jesús Campos <jesus.campos123@icloud.com>
2026-02-28 19:20:03 -0500

    Move snacks keymaps to top of file

--- .config/nvim/lua/plugins/lazyvim/utils/snacks.lua
+++ .config/nvim/lua/plugins/lazyvim/utils/snacks.lua
@@ -1,6 +1,14 @@
 -- Snacks customization
 return {
   "folke/snacks.nvim",
+  -- stylua: ignore
+  keys = {
+    { "<leader>e", function() Snacks.explorer() end, desc = "Explorer" },
+    { "<leader><space>", function() Snacks.picker.files({ cwd = vim.fn.getcwd() }) end, desc = "Find Files" },
+    { "<leader>/", function() Snacks.picker.grep({ cwd = vim.fn.getcwd() }) end, desc = "Grep" },
+    { "<C-/>", function() Snacks.terminal.toggle() end, desc = "Toggle Terminal" },
+    { "<C-\\>", function() Snacks.terminal.open(nil, { win = { position = "float", border = "rounded"} }) end, desc = "Open New Terminal" },
+  },
   opts = {
     dashboard = {
       preset = {
@@ -37,13 +45,5 @@ return {
         },
       },
     },
-  },
-  -- stylua: ignore
-  keys = {
-    { "<leader>e", function() Snacks.explorer() end, desc = "Explorer" },
-    { "<leader><space>", function() Snacks.picker.files({ cwd = vim.fn.getcwd() }) end, desc = "Find Files" },
-    { "<leader>/", function() Snacks.picker.grep({ cwd = vim.fn.getcwd() }) end, desc = "Grep" },
-    { "<C-/>", function() Snacks.terminal.toggle() end, desc = "Toggle Terminal" },
-    { "<C-\\>", function() Snacks.terminal.open(nil, { win = { position = "float", border = "rounded"} }) end, desc = "Open New Terminal" },
   },
 }
