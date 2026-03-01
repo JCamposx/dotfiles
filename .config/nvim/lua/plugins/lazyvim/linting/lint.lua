d0063adf56b0da790a21402622708b4171eee651
Jesús Campos <jesus.campos123@icloud.com>
2026-03-01 10:27:59 -0500

    Remove unnecessary tab level from file

--- .config/nvim/lua/plugins/lazyvim/linting/lint.lua
+++ .config/nvim/lua/plugins/lazyvim/linting/lint.lua
@@ -1,36 +1,34 @@
 -- Lint customization
 return {
-  {
-    "mfussenegger/nvim-lint",
-    opts = {
-      linters_by_ft = {
-        markdown = false,
-      },
+  "mfussenegger/nvim-lint",
+  opts = {
+    linters_by_ft = {
+      markdown = false,
     },
-    init = function()
-      -- Patch golangcilint cwd to the nearest go.mod/go.work dir
-      vim.api.nvim_create_autocmd("User", {
-        pattern = "LazyLoad",
-        callback = function(ev)
-          if ev.data ~= "nvim-lint" then
-            return
-          end
-          local lint = require("lint")
-          local base = lint.linters["golangcilint"]
-          if type(base) ~= "table" then
-            return
-          end
-          lint.linters["golangcilint"] = setmetatable({}, {
-            __index = function(_, k)
-              if k == "cwd" then
-                local fname = vim.api.nvim_buf_get_name(0)
-                return vim.fs.root(fname, { "go.work", "go.mod" }) or vim.fn.getcwd()
-              end
-              return base[k]
-            end,
-          })
-        end,
-      })
-    end,
   },
+  init = function()
+    -- Patch golangcilint cwd to the nearest go.mod/go.work dir
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "LazyLoad",
+      callback = function(ev)
+        if ev.data ~= "nvim-lint" then
+          return
+        end
+        local lint = require("lint")
+        local base = lint.linters["golangcilint"]
+        if type(base) ~= "table" then
+          return
+        end
+        lint.linters["golangcilint"] = setmetatable({}, {
+          __index = function(_, k)
+            if k == "cwd" then
+              local fname = vim.api.nvim_buf_get_name(0)
+              return vim.fs.root(fname, { "go.work", "go.mod" }) or vim.fn.getcwd()
+            end
+            return base[k]
+          end,
+        })
+      end,
+    })
+  end,
 }
