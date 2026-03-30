7bc672b348dca383f1ec8e10d72921ba66bc5ddb
Jesús Campos <jesus.campos123@icloud.com>
2026-03-29 23:29:47 -0500

    Update commasemi keybinds

--- .config/nvim/lua/plugins/user/commasemi.lua
+++ .config/nvim/lua/plugins/user/commasemi.lua
@@ -3,10 +3,10 @@ return {
   "saifulapm/commasemi.nvim",
   event = "VeryLazy",
   keys = {
-    { "<A-,>", "<CMD>CommaToggle<CR>", desc = "Toggle comma", mode = { "n", "i" } },
-    { "<A-,>", ":CommaToggle<CR>gv", desc = "Toggle comma", mode = "x" },
-    { "<A-;>", "<CMD>SemiToggle<CR>", desc = "Toggle semicolon", mode = { "n", "i" } },
-    { "<A-;>", ":SemiToggle<CR>gv", desc = "Toggle comma in visual mode", mode = { "x" } },
+    { "<C-,>", "<CMD>CommaToggle<CR>", desc = "Toggle comma", mode = { "n", "i" } },
+    { "<C-,>", ":CommaToggle<CR>gv", desc = "Toggle comma", mode = "x" },
+    { "<C-;>", "<CMD>SemiToggle<CR>", desc = "Toggle semicolon", mode = { "n", "i" } },
+    { "<C-;>", ":SemiToggle<CR>gv", desc = "Toggle comma in visual mode", mode = { "x" } },
   },
   opts = {
     keymaps = false,
