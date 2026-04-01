ef1f309bef0d353cca2878c56bfc7809d469147c
Jesús Campos <jesus.campos123@icloud.com>
2026-03-31 23:06:22 -0500

    Update branch icon in lualine

--- .config/nvim/lua/plugins/lazyvim/ui/lualine.lua
+++ .config/nvim/lua/plugins/lazyvim/ui/lualine.lua
@@ -26,8 +26,7 @@ return {
       },
       sections = {
         lualine_a = { "mode" },
-        lualine_b = { "branch" },
-
+        lualine_b = { { "branch", icon = "" } },
         lualine_c = {
           {
             "diagnostics",
